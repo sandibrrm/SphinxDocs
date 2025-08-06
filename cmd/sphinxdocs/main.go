c4635721e6096832d4a51b8e74c94a44d1d047ec
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 06:02:24 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/sphinxdocs/main.go
@@ -0,0 +1,23 @@
+// cmd/sphinxdocs/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "sphinxdocs/internal/sphinxdocs"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := sphinxdocs.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
